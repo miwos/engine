b771461f0d0016948407688f401bfd82e46f0c54
arnoson <omasocke@hotmail.de>
2022-12-14 15:37:50 +0100

    chore: represent serialized modules as array

--- src/projects/test/part-1.lua
+++ src/projects/test/part-1.lua
@@ -1,7 +1,7 @@
 return {
   modules = {
-    [1] = { type = 'Input', props = { device = 4, cable = 1 } },
-    [2] = { type = 'Output', props = { device = 1, cable = 1 } },
+    { id = 1, type = 'Input', props = { device = 4, cable = 1 } },
+    { id = 2, type = 'Output', props = { device = 1, cable = 1 } },
   },
   connections = {
     { 1, 1, 2, 1 },
