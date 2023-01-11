26292a80ccf81c40facb0ebc3da266067da51ab5
arnoson <hallo@arno-schlipf.info>
2023-01-11 19:16:56 +0100

    chore: add type annotations

---
+++ src/FileSystem.lua
@@ -0,0 +1,6 @@
+-- Type annotations only, no need to require this file.
+
+-- https://github.com/miwos/firmware/blob/main/include/LuaFileSystem.h
+---@class FileSystem
+---@field listFiles fun(dirName: string)
+FileSystem = {}
