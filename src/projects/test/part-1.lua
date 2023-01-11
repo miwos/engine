f3b4922df1c392da0fe0551094d1af489008ccd2
arnoson <hallo@arno-schlipf.info>
2023-01-11 19:16:15 +0100

    chore: update default project

--- src/projects/test/part-1.lua
+++ src/projects/test/part-1.lua
@@ -1,17 +1,37 @@
 return {
   modules = {
-    { id = 1, type = 'Input', props = { device = 4, cable = 1 } },
-    { id = 2, type = 'Output', props = { device = 1, cable = 1 } },
+    {
+      id = 1,
+      type = 'Input',
+      props = {
+        device = 4,
+        cable = 1,
+      },
+      position = {
+        244,
+        99,
+      },
+    },
+    {
+      id = 2,
+      type = 'Output',
+      props = {
+        device = 1,
+        cable = 1,
+      },
+      position = {
+        468,
+        465,
+      },
+    },
   },
   connections = {
     { 1, 1, 2, 1 },
   },
   mappings = {
-    {
-      { 1, 'device' },
-    },
-    {
-      { 2, 'device' },
+    [1] = {
+      [1] = { 1, 'device' },
+      [2] = { 2, 'device' },
     },
   },
 }
