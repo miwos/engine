bf1e31d976c77d5f05d8317aca869db73fcaec6a
arnoson <hallo@arno-schlipf.info>
2023-01-16 15:02:38 +0100

    chore: improve annotations

--- src/FileSystem.lua
+++ src/FileSystem.lua
@@ -3,4 +3,6 @@
 -- https://github.com/miwos/firmware/blob/main/include/LuaFileSystem.h
 ---@class FileSystem
 ---@field listFiles fun(dirName: string)
+---@field fileExists fun(fileName: string)
+---@field writeFile fun(fileName: string, content: string)
 FileSystem = {}
