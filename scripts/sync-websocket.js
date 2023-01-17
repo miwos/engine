36d70126980bcc1fb08007e8dbdb9b06b5519c08
arnoson <hallo@arno-schlipf.info>
2023-01-17 14:54:39 +0100

    fix: write all files on first connection

--- scripts/sync-websocket.js
+++ scripts/sync-websocket.js
@@ -6,7 +6,17 @@ import { resolve } from 'path';
 const pathToPosix = (path) => path.replace(/\\/g, "/");
 
 const wss = new WebSocketServer({ port: 8080 });
+let initialFilesSynced = false
+
 wss.on('connection', (ws) => {
+  ws.on('message', (buffer) => {
+    const data = JSON.parse(new TextDecoder().decode(buffer))
+    if (data.method === 'deviceConnected' && !initialFilesSynced) {
+      for (let path of filesToSync) syncFile(path, false)
+      initialFilesSynced = true
+    }
+  })
+
   const syncFile = async (path, update = true) => {
     path = pathToPosix(path);
     const content = await fs.readFile(resolve('src', path), "utf8")
@@ -20,8 +30,5 @@ wss.on('connection', (ws) => {
   const filesToSync = [];
   const handleInitialAdd = (path) => filesToSync.push(path);
   watcher.on("add", handleInitialAdd);
-  watcher.on("ready", async () => {
-    watcher.off("add", handleInitialAdd);
-    for (let path of filesToSync) syncFile(path, false)
-  });
+  watcher.on("ready", async () => watcher.off("add", handleInitialAdd));
 })
